979dd483d66bc4e800839ba75fec32d12cb931bf
ashe848 <ashe848@aucklanduni.ac.nz>
2018-08-10 16:24:34 +1200

    Extraction of sensor data and transformation to format we want

--- app/src/main/java/nz/ac/auckland/bocaj/MainActivity.java
+++ app/src/main/java/nz/ac/auckland/bocaj/MainActivity.java
@@ -1,13 +1,34 @@
 package nz.ac.auckland.bocaj;
 
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private int x;
+    private int y;
+
+    //depends on display size
+    private int xCentre;
+    private int yCentre;
+
+    //depends on calibrated pitch and roll (updates if dwell for 5 seconds)
+    private float pitchOffset = 0;
+    private float rollOffset = 0;
+
+    private float[] rotationMatrix = new float[9];
+    private float[] gravity = new float[3];
+    private float[] geomagnetic = new float[3];
+    private float[] orientationValues = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +45,61 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initialiseCalculations();
     }
 
+    private void initialiseCalculations() {
+        Display mdisp = getWindowManager().getDefaultDisplay();
+        Point mdispSize = new Point();
+        mdisp.getSize(mdispSize);
+        int maxX = mdispSize.x;
+        int maxY = mdispSize.y;
+        xCentre = maxX / 2;
+        yCentre = maxY / 2;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                gravity = event.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                geomagnetic = event.values.clone();
+                break;
+            default:
+                return;
+        }
+
+        SensorManager.getRotationMatrix(rotationMatrix, null, gravity, geomagnetic);
+        //TODO may need to transform
+
+        SensorManager.getOrientation(rotationMatrix, orientationValues);
+        float pitch = orientationValues[1];
+        float roll = orientationValues[2];
+
+        float dpitch = pitch - pitchOffset;
+        float droll = roll - rollOffset;
+
+        mapToPointer(dpitch, droll);
+    }
+
+    private void mapToPointer(float dpitch, float droll) {
+        //TODO magic maths happens here
+        int dy = (int) dpitch;
+        int dx = (int) droll;
+
+        x = xCentre + dx;
+        y = yCentre + dy;
+    }
+
+    public Point getClickCoordinates() {
+        return new Point(x, y);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //unused. Do nothing
+    }
 }
