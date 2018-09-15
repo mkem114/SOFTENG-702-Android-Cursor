334589a7bd340e23d4dcd422b9edecab0bff5263
mohan cao <mohancao@yahoo.com.au>
2018-09-15 22:39:13 +1200

    Refactor changes, VOL btn changes, debug msgs, cursor change changes (#15)

--- app/src/main/java/nz/ac/auckland/cursor/MainActivity.java
+++ app/src/main/java/nz/ac/auckland/cursor/MainActivity.java
@@ -2,37 +2,49 @@ package nz.ac.auckland.cursor;
 
 import android.content.Context;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    enum VolumeBtnState {
+        REST,
+        VOL_DOWN_LONG,
+        VOL_DOWN,
+        VOL_UP_LONG,
+        VOL_UP,
+        BOTH,
+    }
+
     private static final float STANDARD_SCREEN_WIDTH = 720;
     private static final int STANDARD_CURSOR_HEIGHT = 60;
     private static final int STANDARD_CURSOR_WIDTH = 70;
 
     private int cursorHeight = STANDARD_CURSOR_HEIGHT;
     private int cursorWidth = STANDARD_CURSOR_WIDTH;
-    private Drawable cursor;
+    private List<Cursor> cursors = new ArrayList<>();
 
-    private static final float CHANGE_SENSITIVITY = 0.08f;
     private static final int DWELL_WINDOW = 80;
     private static final int DWELL_THRESHOLD_MULTIPLIER = 80;
     private static final float MOVEMENT_THRESHOLD_MULTIPLIER = 5;
@@ -62,14 +74,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     //multiplier (z height) for mapping pitch/roll to up/right
     private double pitchMultiplier;
     private double rollMultiplier;
+    // sensitivity too
+    private float changeSensitivity = 0.08f;
 
     private float[] rotationMatrix = new float[9];
     private float[] gravity = new float[3];
     private float[] geomagnetic = new float[3];
     private float[] orientationValues = new float[3];
 
-    private boolean volumeDown = false;
-    private boolean volumeUp = false;
+    private VolumeBtnState volumeBtnState;
+    private boolean disableUpHandler = false;
 
     private SensorManager sensorManager;
 
@@ -78,25 +92,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initialiseCursor();
+        initialiseCursors();
         initialiseCoordinates();
 
+        final TextView textView = (TextView) findViewById(R.id.value_format);
+
         findViewById(android.R.id.content).setOnTouchListener((view, event) -> {
-            Snackbar.make(view, "Click detected at " + event.getX() + "," + event.getY(),
-                    Snackbar.LENGTH_SHORT)
-                    .setAction("Action", null).show();
+            textView.setText(String.format(
+                    Locale.ENGLISH,
+                    "%s detected at %f, %f",
+                    MotionEvent.actionToString(event.getAction()),
+                    event.getX(), event.getY()
+            ));
             view.performClick();
             return true;
         });
     }
 
-    private void initialiseCursor() {
-        cursor = Objects.requireNonNull(ContextCompat.
+    private void initialiseCursors() {
+        Drawable drawable1 = Objects.requireNonNull(ContextCompat.
                 getDrawable(getBaseContext(), R.drawable.cursor));
-        cursor.setBounds(new Rect(xCentre, yCentre, xCentre + cursorWidth,
-                yCentre + cursorHeight));
+        Drawable drawable2 = Objects.requireNonNull(ContextCompat.
+                getDrawable(getBaseContext(), R.drawable.cursor2));
 
-        findViewById(android.R.id.content).getOverlay().add(cursor);
+        Cursor c1 = new Cursor(drawable1, getRealWidth(STANDARD_CURSOR_WIDTH), getRealHeight(STANDARD_CURSOR_HEIGHT), 0, 0);
+        Cursor c2 = new Cursor(drawable2, getRealWidth(60), getRealHeight(60), getRealWidth(30), getRealHeight(30));
+
+        cursors.add(c1);
+        cursors.add(c2);
+        cursors.get(0).updateLocation(xCentre, yCentre);
+        findViewById(android.R.id.content).getOverlay().add(cursors.get(0).getDrawable());
+
     }
 
     private void initialiseCoordinates() {
@@ -129,8 +155,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         screenSizeFactor = displayMetrics.widthPixels / STANDARD_SCREEN_WIDTH;
-        cursorHeight = (int) (screenSizeFactor * STANDARD_CURSOR_HEIGHT);
-        cursorWidth = (int) (screenSizeFactor * STANDARD_CURSOR_WIDTH);
         dwellThreshold = (int) (screenSizeFactor * DWELL_THRESHOLD_MULTIPLIER);
 
         sensorManager.registerListener(this,
@@ -141,6 +165,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 SensorManager.SENSOR_DELAY_GAME);
     }
 
+    private int getRealHeight(int height) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenSizeFactor = displayMetrics.widthPixels / STANDARD_SCREEN_WIDTH;
+        return (int) (screenSizeFactor * height);
+    }
+
+    private int getRealWidth(int width) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenSizeFactor = displayMetrics.widthPixels / STANDARD_SCREEN_WIDTH;
+        return (int) (screenSizeFactor * width);
+    }
+
+    public void toggleNextCursor() {
+        Collections.rotate(cursors, 1);
+        findViewById(android.R.id.content).getOverlay().clear();
+        cursors.get(0).updateLocation(x, y);
+        findViewById(android.R.id.content).getOverlay().add(cursors.get(0).getDrawable());
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -149,45 +194,49 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent keyEvent) {
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-            volumeUp = true;
+        if (volumeBtnState != VolumeBtnState.BOTH && keyCode == KeyEvent.KEYCODE_VOLUME_UP && keyEvent.isLongPress()) {
+            volumeBtnState = VolumeBtnState.VOL_UP_LONG;
+            changeSensitivity = 0.01f;
+            dwellThreshold = 0;
+            return true;
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && volumeBtnState == VolumeBtnState.VOL_DOWN
+                || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && volumeBtnState == VolumeBtnState.VOL_UP) {
+            volumeBtnState = VolumeBtnState.BOTH;
             calibrate();
             return true;
-        }
-
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-            volumeDown = true;
-
-            long downTime = SystemClock.uptimeMillis();
-            long eventTime = SystemClock.uptimeMillis();
-
-            MotionEvent downEvent = MotionEvent.
-                    obtain(downTime, eventTime, MotionEvent.ACTION_DOWN, x, y, 0);
-            MotionEvent upEvent = MotionEvent
-                    .obtain(downTime, eventTime, MotionEvent.ACTION_UP, x, y, 0);
-            findViewById(android.R.id.content).dispatchTouchEvent(downEvent);
-            findViewById(android.R.id.content).dispatchTouchEvent(upEvent);
-            downEvent.recycle();
-            upEvent.recycle();
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && volumeBtnState != VolumeBtnState.VOL_UP
+                && volumeBtnState != VolumeBtnState.VOL_UP_LONG) {
+            volumeBtnState = VolumeBtnState.VOL_UP;
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && volumeBtnState != VolumeBtnState.VOL_DOWN
+                && volumeBtnState != VolumeBtnState.VOL_DOWN_LONG) {
+            volumeBtnState = VolumeBtnState.VOL_DOWN;
+            simulateTouchDown();
             return true;
         }
-
-        return false;
+        return true;
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent keyEvent) {
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-            volumeUp = false;
-            return true;
+        if (disableUpHandler) {
+            disableUpHandler = false;
+            volumeBtnState = VolumeBtnState.REST;
+        } else if (volumeBtnState == VolumeBtnState.BOTH) {
+            disableUpHandler = true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && volumeBtnState == VolumeBtnState.VOL_UP) {
+            volumeBtnState = VolumeBtnState.REST;
+            toggleNextCursor();
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && volumeBtnState == VolumeBtnState.VOL_UP_LONG) {
+            volumeBtnState = VolumeBtnState.REST;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && volumeBtnState == VolumeBtnState.VOL_DOWN) {
+            volumeBtnState = VolumeBtnState.REST;
+            simulateTouchUp();
         }
-
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-            volumeDown = false;
-            return true;
-        }
-
-        return false;
+        changeSensitivity = 0.08f;
+        dwellThreshold = (int) (screenSizeFactor * DWELL_THRESHOLD_MULTIPLIER);
+        return true;
     }
 
     @Override
@@ -232,7 +281,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         for (int i = 0; i < input.length; i++) {
-            previousOutput[i] = previousOutput[i] + CHANGE_SENSITIVITY * (input[i] - previousOutput[i]);
+            previousOutput[i] = previousOutput[i] + changeSensitivity * (input[i] - previousOutput[i]);
         }
 
         return previousOutput;
@@ -261,14 +310,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             x = x > maxX ? maxX : x;
             x = x < 0 ? 0 : x;
         }
-
-        Rect bounds = cursor.copyBounds();
-        bounds.left = x;
-        bounds.top = y;
-        bounds.right = x + cursorWidth;
-        bounds.bottom = y + cursorHeight;
-        cursor.setBounds(bounds);
-        findViewById(android.R.id.content).invalidate();
+        if (volumeBtnState == VolumeBtnState.VOL_DOWN) simulateTouchMove();
+        cursors.get(0).updateLocation(x, y);
     }
 
     private boolean isDwell(int newX, int newY) {
@@ -291,5 +334,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         } else {
             return false;
         }
+    }
+
+    /**
+     * Scooooooooreeeeee!!!
+     */
+    public void simulateTouchDown() {
+        long upTime = SystemClock.uptimeMillis();
+        long eventTime = SystemClock.uptimeMillis();
+        MotionEvent downEvent = MotionEvent.
+                obtain(upTime, eventTime, MotionEvent.ACTION_DOWN, (float)x, (float)y, 0);
+        findViewById(android.R.id.content).dispatchTouchEvent(downEvent);
+        downEvent.recycle();
+    }
+
+    /**
+     * All natural
+     */
+    public void simulateTouchUp() {
+        long downTime = SystemClock.uptimeMillis();
+        long eventTime = SystemClock.uptimeMillis();
+        MotionEvent upEvent = MotionEvent
+                .obtain(downTime, eventTime, MotionEvent.ACTION_UP, (float)x, (float)y, 0);
+        findViewById(android.R.id.content).dispatchTouchEvent(upEvent);
+        upEvent.recycle();
+    }
+
+    /**
+     * Isn't that some sort of chess rule?
+     */
+    public void simulateTouchMove() {
+        long moveTime = SystemClock.uptimeMillis();
+        long eventTime = SystemClock.uptimeMillis();
+        MotionEvent moveEvent = MotionEvent
+                .obtain(moveTime, eventTime, MotionEvent.ACTION_MOVE, (float)x, (float)y, 0);
+        findViewById(android.R.id.content).dispatchTouchEvent(moveEvent);
+        moveEvent.recycle();
     }
 }
