a1cd6d88daf848d66f79d95c0321463adf120af9
mohan-cao <mohancao@yahoo.com.au>
2018-09-16 21:50:10 +1200

    Resolved issues with building

--- Usability/app/src/main/java/nz/ac/auckland/usabilitytest/MainActivity.java
+++ Usability/app/src/main/java/nz/ac/auckland/usabilitytest/MainActivity.java
@@ -1,45 +1,25 @@
-package nz.ac.auckland.cursor;
+package nz.ac.auckland.usabilitytest;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.pm.ActivityInfo;
-import android.graphics.Point;
-import android.graphics.drawable.Drawable;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
-import android.view.Display;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
 import java.util.Random;
 
+import nz.ac.auckland.cursor.CursorOverlay;
 
 public class MainActivity extends CursorOverlay implements SensorEventListener {
     public static final int NUM_TO_PRESS = 20;
@@ -131,7 +111,7 @@ public class MainActivity extends CursorOverlay implements SensorEventListener {
     private void printToLogs(String message) {
         File backupPath = Environment.getExternalStorageDirectory();
         backupPath = new File(backupPath.getPath() +
-                "/Android/data/nz.ac.auckland.cursor/logs");
+                "/Android/data/nz.ac.auckland.usabilitytest/logs");
         if (!backupPath.exists()) {
             if (!backupPath.mkdirs()) {
                 AlertDialog.Builder delmessagebuilder = new AlertDialog.Builder(this);
