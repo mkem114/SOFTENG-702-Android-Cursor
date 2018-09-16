983d10403596ec4da008ed651a4fcea696a3c576
mohan-cao <mohancao@yahoo.com.au>
2018-09-16 21:36:11 +1200

    Included new cursor library

--- Usability/README.md
+++
@@ -1,4 +0,0 @@
-# project-13-a SOFTENG-702-Android-Cursor
-[![Build Status](https://travis-ci.com/mkem114/SOFTENG-702-Android-Cursor.svg?token=4tn5PhULbqssssJGM5Gs&branch=master)](https://travis-ci.com/mkem114/SOFTENG-702-Android-Cursor)
-
-Pointing and clicking with accelerometers
--- Usability/cursor/src/main/java/nz/ac/auckland/cursor/Cursor.java
+++ Usability/cursor/src/main/java/nz/ac/auckland/cursor/Cursor.java
@@ -18,8 +18,6 @@ public class Cursor {
         this.height = height;
         this.centerX = centerX;
         this.centerY = centerY;
-        Log.e("Cursor", String.format("Width: %d, Height: %d", width, height));
-
     }
 
     public Drawable getDrawable() {
@@ -28,7 +26,6 @@ public class Cursor {
 
     public void updateLocation(int x, int y) {
         Rect bounds = cursor.copyBounds();
-        Log.e("Cursor", String.format("Top: %s, Left: %s, Right: %s, Bottom: %s", bounds.top, bounds.left, bounds.right, bounds.bottom));
         bounds.left = x - centerX;
         bounds.top = y - centerY;
         bounds.right = x + width - centerX;
